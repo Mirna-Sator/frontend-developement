c510b4bf58cebcbefa67ac3f4cab6bbf8cb0a41f
Mirna-Sator <70636882+Mirna-Sator@users.noreply.github.com>
2020-10-20 16:26:24 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+var menuSection = document.querySelector("#menu");
+var buttonHamburgermenu = document.querySelector("#hamburgerbutton");
+var sluitenMenu = document.querySelector("#sluiten");
+
+
+function menuopen(){
+	menuSection.classList.toggle("menuasosopen");
+}
+
+buttonHamburgermenu.addEventListener("click", menuopen);
+sluitenMenu.addEventListener("click", menuopen);
